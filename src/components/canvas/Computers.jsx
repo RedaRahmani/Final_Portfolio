0f628c4bdf45a5c1b5ceaf7d6d403368a9c77fa5
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2024-04-24 10:41:14 +0100

    fix computer display errors

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,8 +9,12 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight
+  {/* Hemisphere Light */}
+  <hemisphereLight intensity={1}  groundColor='black'  />
+
+  <directionalLight intensity={0.5}  position={[10,20,10]} castShadow />
+  {/* Spot Light */}
+  <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
@@ -18,22 +22,17 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
-      <spotLight 
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <primitive
+  {/* Point Light */}
+  <pointLight intensity={1}   />
+
+  {/* Primitive Object */}
+  <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
+      />groundColor={0x808080}
+</mesh>
   );
 };
 
