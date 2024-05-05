8c6674105d34de468d87d1e36e584ea51745cf71
khadira1937 <okhadira@gmail.com>
2024-05-06 00:56:03 +0200

    update project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>RAHMANI</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I'm a full-stack software engineer with experience in website development, network security, network administration, and software development. <br className='sm:block hidden' />
+          I'm a full-stack software engineer with experience in website development, network security, network administration, and software development.  <br className='sm:block hidden' />
           Check out my projects and skills.
           </p>
         </div>
